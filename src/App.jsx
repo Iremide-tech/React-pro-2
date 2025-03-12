0dc51b0f97b8d3465796c7dba318b8be142134f8
Iremide-tech <iresamuel226@gmail.com>
2025-03-12 20:40:41 +0100

    Added a form added the styling of the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [values, setValues] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    contact: '',
+    gender: '',
+    subject: '',
+    description: '',
+   })
 
+const handleChanges = (e) => {
+   setValues({...values, [e.target.name]:[e.target.value]})
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  console.log(values)
+}
+
+const ResetFun = () => {
+  setValues({firstname: '', lastname: '', email: ''})
+}
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <h1>Resume Builder</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="firstname">First Name</label>
+          <input type='text' placeholder='Enter your name' name='fistname'
+          onChange={(e) => handleChanges(e)} required value={values.firstname}
+          />
+             <br />
+          <label htmlFor="lastname">Last Name</label>
+          <input type='text' placeholder='Enter your last name' name='lastname'
+           onChange={(e) => handleChanges(e)} required value={values.lastname}/> 
+             <br />
+           <label htmlFor="email">Email</label>
+          <input type='text' placeholder='Type your email' name='email'
+           onChange={(e) => handleChanges(e)} required/>
+       <br />
+          <label htmlFor="contact">Contact</label>
+          <input type='number' placeholder='Phone no' name='contact'
+           onChange={(e) => handleChanges(e)} required/>
+       <br />
+   <label htmlFor="gender">Gender</label>
+   <input type="radio" 
+    onChange={(e) => handleChanges(e)}/> Male
+   <input type="radio" 
+    onChange={(e) => handleChanges(e)}/> Female
+   <input type="radio"
+    onChange={(e) => handleChanges(e)} /> Other
+         <br />
+   <label htmlFor="subject">Subject</label>
+   <select name="subject" id="subject"  onChange={(e) => handleChanges(e)}>
+    <option value="Maths">Maths</option>
+    <option value="Physics">Physics</option>
+    <option value="English">English</option>
+   </select>
+  <br />
+   <label htmlFor="description">Description</label>
+   <textarea name="description" id="description" cols="30" rows="10"
+    onChange={(e) => handleChanges(e)} placeholder='About you'></textarea>
+
+<button type="submit">Submit</button>
+<button type="reset" onClick={ResetFun}>Reset</button>
+   </form>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
