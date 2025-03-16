f8bfe7915ce1d7dd9befa405b21b94b97f13e0bf
Iremide-tech <iresamuel226@gmail.com>
2025-03-16 18:18:49 +0100

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Form from "./Form";
 import Secondpage from "./Innerpage/Secondpage";
+import Navbar from "./Innerpage/Navbar";
 
 
 function App() {
 
   return (
     <>
+    <Navbar/>
     <Form/>
     <Secondpage/>
+  
     </>   
   )
 }
